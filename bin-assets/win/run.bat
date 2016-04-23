c6cc3b28d27b6b78620ac1f1115ef96a3c0d12b9
Rodrigo Lopez <rodrigolopezpeker@gmail.com>
2016-04-23 20:20:10 -0300

    fix error to open the iexplorer more than once

--- bin-assets/win/run.bat
+++ bin-assets/win/run.bat
@@ -1,6 +1,8 @@
 rem start WebViewWpfApp.exe -resW 1920 -resH 1080 -locX 0 -locY 0 -sendPort 9010 -recvPort 9011 -debug
 @echo off
-start iexplore -k http://10.0.1.5:8888/KaonCityscape
-rem start iexplore http://10.0.1.5:8888/KaonCityscape
+tasklist /fi "imagename eq iexplore.exe" > nul
+if errorlevel 1 start iexplore -k http://10.0.1.5:8888/KaonCityscape
+rem if errorlevel 1 start iexplore http://10.0.1.5:8888/KaonCityscape
+exit
 
 
